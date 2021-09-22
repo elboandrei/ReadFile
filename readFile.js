f9b3908262a1fa4ac640321f19bd03ec4f5d0579
elboandrei <andreijce@gmail.com>
2021-09-21 20:10:37 -0400

    Excercise 2

--- readFile.js
+++ readFile.js
@@ -4,3 +4,7 @@ fs.readFile('lorem-ipsum.txt', 'utf8', (err, data) => {
     if (err) throw  (err);
     console.log(data);
 });
+
+fs.rename('lorem-ipsum.txt', 'gibberish.txt', (err) => {
+    if (err) throw (err)
+})
